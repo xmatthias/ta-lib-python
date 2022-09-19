4161a83bb8514ff537a3fe02b36a177c9f2c0541
Matthias <xmatthias@outlook.com>
2022-09-19 14:27:34 +0000

    Update test to 0.5.0

--- tests/test_func.py
+++ tests/test_func.py
@@ -11,7 +11,7 @@ from talib import func
 
 
 def test_talib_version():
-    assert talib.__ta_version__[:5] == b'0.4.0'
+    assert talib.__ta_version__[:5] == b'0.5.0'
 
 
 def test_num_functions():
