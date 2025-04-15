3e81dc36383d00d34e1ec93e00c58e66b16254d1
Matthias <xmatthias@outlook.com>
2025-04-15 04:52:15 +0000

    chore: update ta-lib from source to use github sources

--- tools/build_talib_from_source.bash
+++ tools/build_talib_from_source.bash
@@ -7,9 +7,10 @@ if [[ -z $1 ]]; then
 fi
 
 DEPS_DIR=$1
+VERSION=0.4.0
 
-TA_LIB_TGZ="ta-lib-0.4.0-src.tar.gz"
-TA_LIB_URL="http://prdownloads.sourceforge.net/ta-lib/$TA_LIB_TGZ"
+TA_LIB_TGZ="ta-lib-0.${VERSION}-src.tar.gz"
+TA_LIB_URL="https://github.com/TA-Lib/ta-lib/releases/download/v${VERSION}/$TA_LIB_TGZ"
 
 if [[ -d $DEPS_DIR/lib ]]; then
   echo "Already built"
@@ -20,7 +21,7 @@ wget -O "$DEPS_DIR/tmp/$TA_LIB_TGZ" $TA_LIB_URL
 pushd $DEPS_DIR/tmp
 tar -zxvf $TA_LIB_TGZ
 popd
-pushd $DEPS_DIR/tmp/ta-lib
+pushd $DEPS_DIR/tmp/ta-lib-${VERSION}
 ./configure --prefix=$DEPS_DIR
 make install
 popd
